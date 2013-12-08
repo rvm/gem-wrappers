af68570579c386ad1f6c3c9c4bc7f26ec62575cc
Michal Papis <mpapis@gmail.com>
2013-12-08 09:26:41 +0100

    initial rubygems plugin scaffold

---
+++ lib/gem/wrappers/specification.rb
@@ -0,0 +1,19 @@
+module Gem
+  module Wrappers
+    module Specification
+      def self.find
+        @executable_hooks_spec ||=
+          if Gem::Specification.respond_to?(:find_by_name)
+            Gem::Specification.find_by_name("gem-wrappers")
+          else
+            Gem.source_index.find_name("gem-wrappers").last
+          end
+      rescue Gem::LoadError
+        nil
+      end
+      def self.version
+        find ? find.version.to_s : nil
+      end
+    end
+  end
+end
---
+++ lib/rubygems_plugin.rb
@@ -0,0 +1,18 @@
+# Simulate require_relative - it's required as the plugin can be called in wrong version or from bundler.
+require File.expand_path('../gem/wrappers/specification.rb', __FILE__)
+
+called_path, called_version = __FILE__.match(/^(.*\/gem-wrappers-([^\/]+)\/lib).*$/)[1..2]
+
+# continue only if loaded and called versions all the same, and not shared gems disabled in bundler
+if
+  ( $:.include?(called_path) || Gem::Wrappes::Specification.version == called_version ) and
+  ( !defined?(Bundler) || ( defined?(Bundler) && Bundler::SharedHelpers.in_bundle? && !Bundler.settings[:disable_shared_gems]) )
+
+  require File.expand_path('../gem/wrappers/installer.rb', __FILE__)
+  require File.expand_path('../gem/wrappers/regenerate_wrappers_command.rb', __FILE__)
+
+  Gem.post_install do |installer|
+    Gem::Wrappes::Installer.new(installer).install
+  end
+  Gem::CommandManager.instance.register_command :regenerate_wrappers
+end
